ef1881d8f77f8364f5c6e76c38436282cb13f5a2
Neil J. Gunther <DrQz@users.noreply.github.com>
2017-12-04 19:20:33 -0800

    Added

--- pdqx.pl
+++ pdqx.pl
@@ -8,13 +8,13 @@ use Data::Dumper;
 
 # open Report file and scan for line containing the following keywords ...
 my $keywords = quotemeta "Node Sched Resource   Workload";
-my $file = "diskopt-rpt.txt";
-my $infile = "$file";
+my $file = "diskopt";
+my $infile = "$file-rpt.txt";
 open(INFILE, "< $infile") or die "can't open $infile: $!";
 
 my @chunks;
 my $started = 0;
-my %nodekv; # hash of lists
+my %nodeKV; # hash of lists
 
 
 while (<INFILE>) {
@@ -39,7 +39,7 @@ while (<INFILE>) {
 	    @chunks = split(' ', $x);  # matches any whitespace
 		if ($chunks[5] > 0) {
 			# must have non-zero service demand to get hashed
-			push(@{$nodekv{$chunks[2]}}, $chunks[3]);
+			push(@{$nodeKV{$chunks[2]}}, $chunks[3]);
 		}
 	}
 }
@@ -48,11 +48,33 @@ close(INFILE) or die "can't close $infile: $!";
 
 # diagnostics
 #print "Chunks: @chunks\n";
-print Dumper(\%nodekv);
+#print Dumper(\%nodeKV);
 
 
+# GV format template
 
+my $new = "$file.dot";
+open(NEW, "> $new") or die "can't open $new: $!";
 
+print NEW "digraph G {\n";
+print NEW "\tcompound=true;\n";
+print NEW "\tranksep=1.25;\n";
+print NEW "\trankdir=LR;\n";
+print NEW "\tnode [shape=plaintext, fontsize=16, label=\"\"];\n";
 
+print NEW "\tsrc [label=\"Source\"];\n";
+print NEW "\tsnk [label=\"Sink\"];\n";
 
+for my $key ( keys %nodeKV ) {
+    print NEW "\t$key [shape=none, label=\"$key\", image=\"node-single.png\"];\n";
+}
+
+for my $key ( keys %nodeKV ) {
+    print NEW "\tsrc -> $key;\n";
+    print NEW "\t$key -> snk;\n";
+}
+
+print NEW "}\n";
+
+close(NEW) or die "can't close $new: $!";
 
